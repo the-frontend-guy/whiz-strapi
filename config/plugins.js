de2f9c53edcd60e522f3e5968ce307528babcd5c
vikas pal <vikas.p@bewdigital.com>
2020-07-07 02:14:28 +0530

    configured smtp

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    provider: 'Nodemailer Gmail OAuth2',
+    config: {
+      username: 'vikas',
+      clientId: '428733288893-qhlhpdc48543equu1mgdq6jd50gtvqhi.apps.googleusercontent.com',
+      clientSecret: '-KGwzn3ciiGybnc7PSEp8fPu',
+      refreshToken: '1//04lIKdjdIHIs5CgYIARAAGAQSNwF-L9IrpMeJXd_J_4kaEdozZv__6k7xvXWF7FDF0mN9TL17DAQxfR2a6V3GuMIzzd4UVKhl91s',
+      accessToken: 'ya29.a0AfH6SMBtg1QvBkTLtV8hM3CI_BZpmVvvENbXCdtQSMi96ljfHtTATZleUU5NK4GA4xSawLHAvF09cdvcYbQkL1sGroKMycPyKCLR50buiA5XN7EpAStKDM4nvFDAQ55sD0Zym9HxlxC2L1GSjKeGHTOvJF0wkTx7hoE',
+    },
+    
+  },
+  // ...
+});
